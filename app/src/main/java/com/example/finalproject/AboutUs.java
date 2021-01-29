c3b39da6d0e427bd148d8975d34eb4bb747f0f04
ziwenma <ziwenemma2017@gmail.com>
2021-01-29 17:16:16 -0500

    error checking

--- app/src/main/java/com/example/finalproject/AboutUs.java
+++ app/src/main/java/com/example/finalproject/AboutUs.java
@@ -14,7 +14,8 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 
-
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 
 public class AboutUs extends AppCompatActivity {
@@ -23,5 +24,36 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+
+        Element adsElement = new Element();
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.logositter)
+                .setDescription(" our application is use for find a babysitter for parents also privide a job for babysitter")
+                .addItem(new Element().setTitle("Little Laughs"))
+                .addGroup("Learn more about US!")
+                .addEmail("ziwenemma2017@gmail.com")
+                .addWebsite("https://github.com/ziwenemma/FinalProject.git")
+                .addYoutube("https://www.youtube.com/")
+                .addInstagram("https://www.instagram.com/?hl=en")
+                .addFacebook("https://www.facebook.com/")
+                .addItem(createCopyright())
+                .create();
+        setContentView(aboutPage);
+    }
+    private Element createCopyright()
+    {
+        Element copyright = new Element();
+        @SuppressLint("DefaultLocale") final String copyrightString = String.format("2021 little laughs(412 group)", Calendar.getInstance().get(Calendar.YEAR));
+        copyright.setTitle(copyrightString);
+        copyright.setGravity(Gravity.CENTER);
+        copyright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AboutUs.this,copyrightString,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyright;
     }
 }
