878ba659c572a86b4065d231acaef165362364d8
ziwenemma <ziwenemma2017@gmail.com>
2021-02-02 20:36:16 -0500

    layout of post information and information page for parents

--- app/src/main/java/com/example/finalproject/ParentInformation.java
+++ app/src/main/java/com/example/finalproject/ParentInformation.java
@@ -2,13 +2,29 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ParentInformation extends AppCompatActivity {
+Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_information);
+
+       btn=findViewById(R.id.ChangeParentInfo);
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent=new Intent(ParentInformation.this,PostInfoParent.class);
+               startActivity(intent);
+           }
+       });
     }
+
+
 }
