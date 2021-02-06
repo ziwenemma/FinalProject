c99ddf88f90f96880b20c4e74d9209620ece77f5
Sukhdeepkaur19960 <61361050+Sukhdeepkaur19960@users.noreply.github.com>
2021-02-05 19:12:43 -0500

    Update MainPageSitter.java
    retrive the database postSitter information

--- app/src/main/java/com/example/finalproject/MainPageSitter.java
+++ app/src/main/java/com/example/finalproject/MainPageSitter.java
@@ -1,11 +1,16 @@
 package com.example.finalproject;
 
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -15,16 +20,36 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.ListFragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import javax.annotation.Nullable;
 
 public class MainPageSitter extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    TextView SitterGender,SitterName,SitterAge,SitterEmail,SitterPhone,SitterCity,SitterDesc,SitterRate;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
     Button btn;
-
+    ImageView SitterImageView;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseUser sitteruser;
+    StorageReference storageReference;
 
 
     @Override
@@ -33,11 +58,72 @@ public class MainPageSitter extends AppCompatActivity implements NavigationView.
         setContentView(R.layout.activity_main_page_sitter);
         btn=findViewById(R.id.sitterupdateto);
 
+
+        SitterGender=findViewById(R.id.babysitter_gender);
+        SitterName = findViewById(R.id.sitter_name);
+        SitterAge = findViewById(R.id.babysitte_age);
+        SitterCity =findViewById(R.id.babysitte_city);
+        SitterEmail=findViewById(R.id.babysitter_email);
+        SitterPhone=findViewById(R.id.babysitter_ph);
+        SitterDesc=findViewById(R.id.babysitte_des);
+        SitterRate=findViewById(R.id.babysitter_rate);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        SitterImageView=findViewById(R.id.sitter_image);
+
+
+        StorageReference profileRef = storageReference.child("parentuser/"+fAuth.getCurrentUser().getUid()+"/profileimage.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(SitterImageView);
+            }
+        });
+
+        userId = fAuth.getCurrentUser().getUid();
+        sitteruser = fAuth.getCurrentUser();
+
+
+        DocumentReference documentReference = fStore.collection("babysitterusers").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(documentSnapshot.exists()){
+                    SitterName.setText(documentSnapshot.getString("BabySitterName"));
+                    SitterAge.setText(documentSnapshot.getString("BabySitterAge"));
+                    SitterPhone.setText(documentSnapshot.getString("BabySitterPhone"));
+                    SitterEmail.setText(documentSnapshot.getString("BabySitterEmail"));
+                    SitterCity.setText(documentSnapshot.getString("BabySitterCity"));
+                    SitterGender.setText(documentSnapshot.getString("BabySitterGender"));
+                    SitterDesc.setText(documentSnapshot.getString("BabySitterDesc"));
+                    SitterRate.setText(documentSnapshot.getString("BabySitterRate"));
+
+                }else {
+                    Log.d("tag", "onEvent: Document do not exists");
+                }
+            }
+        });
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainPageSitter.this,post_my_profile.class);
-                startActivity(intent);
+                Intent i = new Intent(v.getContext(),post_my_profile.class);
+                i.putExtra("BabySitterName",SitterName.getText().toString());
+                i.putExtra("BabySitterAge",SitterAge.getText().toString());
+                i.putExtra("BabySitterCity",SitterCity.getText().toString());
+                i.putExtra("BabySitterRate",SitterRate.getText().toString());
+                i.putExtra("BabySitterDesc",SitterDesc.getText().toString());
+                i.putExtra("BabySitterGender",SitterGender.getText().toString());
+                i.putExtra("BabySitterEmail",SitterEmail.getText().toString());
+                i.putExtra("BabySitterPhone",SitterPhone.getText().toString());
+                startActivity(i);
+
             }
         });
 
@@ -59,6 +145,12 @@ public class MainPageSitter extends AppCompatActivity implements NavigationView.
         NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view_sitter);
         navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+
+    public void changeProfile(View view) {
+        Intent intent=new Intent(this, post_my_profile.class);
+        startActivity(intent);
     }
 
     @Override
