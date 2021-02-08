09ddcd0d1478134fad1368eff96245a230f2d760
Sukhmanjot Kaur Gill <61361043+Sukhmanjot25@users.noreply.github.com>
2021-02-08 16:58:24 -0500

    Update AppointmentInfo.java
    combine two together parentinfo and babysitter info

--- app/src/main/java/com/example/finalproject/AppointmentInfo.java
+++ app/src/main/java/com/example/finalproject/AppointmentInfo.java
@@ -5,14 +5,77 @@ public class AppointmentInfo {
 
     }
 
-    String babysitter_id, BabysitterName, BabysitterPhone, BabysitterEmail, BabysitterRate;
+    String babysitter_id, BabysitterName, BabysitterPhone, BabysitterEmail, BabysitterRate,ParentName,ParentEmail,ParentPhone,ParentChildAge,ParentReq,ParentAdd,ChildGender;
 
-    public AppointmentInfo(String babysitter_id, String babysitterName, String babysitterPhone, String babysitterEmail, String babysitterRate) {
+    public AppointmentInfo(String babysitter_id, String babysitterName, String babysitterPhone, String babysitterEmail, String babysitterRate, String parentName, String parentEmail, String parentPhone, String parentChildAge, String parentReq, String parentAdd, String childGender) {
         this.babysitter_id = babysitter_id;
         BabysitterName = babysitterName;
         BabysitterPhone = babysitterPhone;
         BabysitterEmail = babysitterEmail;
         BabysitterRate = babysitterRate;
+        ParentName = parentName;
+        ParentEmail = parentEmail;
+        ParentPhone = parentPhone;
+        ParentChildAge = parentChildAge;
+        ParentReq = parentReq;
+        ParentAdd = parentAdd;
+        ChildGender = childGender;
+    }
+
+    public String getParentName() {
+        return ParentName;
+    }
+
+    public void setParentName(String parentName) {
+        ParentName = parentName;
+    }
+
+    public String getParentEmail() {
+        return ParentEmail;
+    }
+
+    public void setParentEmail(String parentEmail) {
+        ParentEmail = parentEmail;
+    }
+
+    public String getParentPhone() {
+        return ParentPhone;
+    }
+
+    public void setParentPhone(String parentPhone) {
+        ParentPhone = parentPhone;
+    }
+
+    public String getParentChildAge() {
+        return ParentChildAge;
+    }
+
+    public void setParentChildAge(String parentChildAge) {
+        ParentChildAge = parentChildAge;
+    }
+
+    public String getParentReq() {
+        return ParentReq;
+    }
+
+    public void setParentReq(String parentReq) {
+        ParentReq = parentReq;
+    }
+
+    public String getParentAdd() {
+        return ParentAdd;
+    }
+
+    public void setParentAdd(String parentAdd) {
+        ParentAdd = parentAdd;
+    }
+
+    public String getChildGender() {
+        return ChildGender;
+    }
+
+    public void setChildGender(String childGender) {
+        ChildGender = childGender;
     }
 
     public String getBabysitter_id() {
